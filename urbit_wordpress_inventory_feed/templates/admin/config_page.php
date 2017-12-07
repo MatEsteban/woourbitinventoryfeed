b8652953d0085bdb5ce5443482df0fefd9d9e00a
Unknown <mathieu@urbit.com>
2017-12-07 10:13:24 +0100

    Remove unecessary class

--- urbit_wordpress_inventory_feed/templates/admin/config_page.php
+++ urbit_wordpress_inventory_feed/templates/admin/config_page.php
@@ -1,22 +1,24 @@
 <div class="wrap">
-    <h2><?php echo get_admin_page_title() ?></h2>
+    <div class="bootstrap-wrapper">
+        <h2><?php echo get_admin_page_title() ?></h2>
 
-    <form action="options.php" method="POST" class="urbit_productfeed_form">
-        <?php
-            settings_fields($option_group);
+        <form action="options.php" method="POST" class="urbit_inventoryfeed_form">
+            <?php
+                settings_fields($option_group);
 
-            /** @var UPF_Admin_Settings_Section $section */
-            foreach ($sections as $section) {
-                do_settings_sections($section->getPageId());
-            }
+                /** @var UIF_Admin_Settings_Section $section */
+                foreach ($sections as $section) {
+                    do_settings_sections($section->getPageId());
+                }
 
-            submit_button();
-        ?>
-    </form>
+                submit_button();
+            ?>
+        </form>
+    </div>
 </div>
 
 <style>
-    .urbit_productfeed_form select {
+    .urbit_inventoryfeed_form select {
         min-width: 190px;
     }
 </style>
